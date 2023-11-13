c713e748bb51ab258b2b9f3bfb8ad336f9d4ba7b
hankh <hankh.dev@gmail.com>
2023-11-13 12:09:05 +0900

    chore

--- src/main/java/com/example/springtest/restservice/GreetingController.java
+++ src/main/java/com/example/springtest/restservice/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.springtest.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,12 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println("GET /greeting, port: " + port);
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
