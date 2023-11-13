c92436d85df33651afc62ba5d7c4522625f1355d
hankh <hankh.dev@gmail.com>
2023-11-13 12:31:32 +0900

    chore

--- src/main/java/com/example/springtest/restservice/GreetingController.java
+++ src/main/java/com/example/springtest/restservice/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,13 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println("GET /greeting, port: " + port);
         Executor executor = Executors.newFixedThreadPool(30);
-
         CompletableFuture.runAsync(() -> {
             try {
                 for(int i = 0; i < 100; i++) {
