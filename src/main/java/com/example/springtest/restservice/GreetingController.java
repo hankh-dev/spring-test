8420be86138e4cd1126a78404cb5c535f8d0a02d
hankh <hankh.dev@gmail.com>
2023-11-27 12:11:08 +0900

    Executor executor = Executors.newFixedThreadPool(10);

--- src/main/java/com/example/springtest/restservice/GreetingController.java
+++ src/main/java/com/example/springtest/restservice/GreetingController.java
@@ -22,7 +22,7 @@ public class GreetingController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         System.out.println("GET /greeting, port: " + port);
-        Executor executor = Executors.newFixedThreadPool(30);
+        Executor executor = Executors.newFixedThreadPool(10);
         CompletableFuture.runAsync(() -> {
             try {
                 for(int i = 0; i < 100; i++) {
